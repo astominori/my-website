ded81738980cab0edc16ccb954d0e22027e350ce
minori <minori.astro317@gmail.com>
2021-08-19 20:55:14 +0900

    20210819/カードが浮き出てくるを実装

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-var cards = document.getElementsByClassName('link-card');
-for (var i=0; i < cards.length; i++) {
-    cards[i].addEventListener('click', function() {
-        cardUrl = this.href
-        location.url = cardUrl;
-    });
-};
+'use strict';
+
+let fadeInTarget = document.querySelectorAll('.fade-in');
+window.addEventListener('scroll', () => {
+    for (let i = 0; i < fadeInTarget.length; i++){
+        const rect = fadeInTarget[i].getBoundingClientRect().top;
+        const scroll = window.pageYOffset || document.documentElement.scrollTop;
+        const offset = rect + scroll;
+        const windowHeight = window.innerHeight;
+        if( scroll > offset - windowHeight + 150) {
+            fadeInTarget[i].classList.add('scroll-in');
+        }
+    }
+});
